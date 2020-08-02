f2ddd3bbcb5a636f94f24b01f5a579ade94e33a2
Bryssinck,Robbe R <robbebryssinck@gmail.com>
2020-08-02 17:06:25 +0200

    Database and working API requests

--- CSGOTrackerAPI/CSGOTrackerAPI/Startup.cs
+++ CSGOTrackerAPI/CSGOTrackerAPI/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using CSGOTrackerAPI.Models;
 
 namespace CSGOTrackerAPI
 {
@@ -25,6 +27,8 @@ namespace CSGOTrackerAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<GameContext>(opt =>
+                opt.UseInMemoryDatabase("Games"));
             services.AddControllers();
         }
 
