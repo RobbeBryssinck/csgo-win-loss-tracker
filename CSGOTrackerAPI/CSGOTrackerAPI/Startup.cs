dae34f143f2d91726919fcb386aa55281873c452
Bryssinck,Robbe R <robbebryssinck@gmail.com>
2020-08-02 20:13:33 +0200

    GameDTO

--- CSGOTrackerAPI/CSGOTrackerAPI/Startup.cs
+++ CSGOTrackerAPI/CSGOTrackerAPI/Startup.cs
@@ -40,6 +40,9 @@ namespace CSGOTrackerAPI
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
